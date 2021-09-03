0e368f0d5ec012b71b63b5e960d2feca8ea80e0d
Devin Mork <devin.mork@gmail.com>
2021-09-03 12:31:35 -0500

    Add add_page()

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -84,4 +84,7 @@ def add_category(request):
   # Will handle the bad form, new form, or no form supplied cases.
   # Render the form with error messages (if any).
   return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request):
+  pass
   
