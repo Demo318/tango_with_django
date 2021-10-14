269b2278e022323fa327c67f6d80711f0c836254
PhiIotaNuAlphaLambda <devin@finalsigma.io>
2021-10-13 19:43:48 -0500

    Fixed comment.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -30,7 +30,7 @@ def about(request):
   return render(request, 'rango/about.html', context = context_dict)
 
 def show_category(request, category_name_slug):
-  # Createa  context dictionary which we can pass
+  # Create a context dictionary which we can pass
   # to the template rendering engine.
   context_dict = {}
 
