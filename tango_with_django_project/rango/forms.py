4a1976c142a65f135a92598662fa3041b0790e3a
PhiIotaNuAlphaLambda <devin@finalsigma.io>
2021-11-30 13:55:09 -0600

    Create registration functionality.

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,9 +1,10 @@
 ﻿from django import forms
-from rango.models import Page, Category
+from rango.models import Page, Category, UserProfile, UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
   name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,
-                         help_text="Please enter the categoryname.")
+                         help_text="Please enter the category name.")
   views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
   likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
   slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -45,3 +46,14 @@ class PageForm(forms.ModelForm):
 
     return cleaned_data
 
+class UserForm(forms.ModelForm):
+  password = forms.CharField(widget=forms.PasswordInput())
+
+  class Meta:
+    model = User
+    fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+  class Meta:
+    model = UserProfile
+    fields = ('website', 'picture')
